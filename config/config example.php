b25625885a8db0092695f72249766dc86ff6e008
klocstharss <anonimushackerg1@gmail.com>
2025-05-30 12:17:22 -0500

    prueba2

---
+++ config/config example.php
@@ -0,0 +1,10 @@
+<?php
+const BD_HOST = 'localhost';
+const BD_NAME = '';
+const BD_USER = '';
+const BD_PASSWORD = '';
+const BD_charset = 'utf8';
+//ingresar link de proyecto
+const BASE_URL= '';
+
+?>
